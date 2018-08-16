38e04e0c15cf8bf04c0062328d578723fb028c66
LegendaryZealot <875626439@qq.com>
2018-08-16 14:27:16 +0800

    add harris

---
+++ harris/main.py
@@ -0,0 +1,16 @@
+import cv2.cv2 as cv
+import numpy as np
+
+img=cv.imread("test.jpg")
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+cv.imshow("Result",img)
+cv.waitKey(0)
